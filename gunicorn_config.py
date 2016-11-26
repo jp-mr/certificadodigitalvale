7c277f4137bcc2539c14af87feb050cd16001672
Michel <michel.rodrigues86@yahoo.com.br>
2016-11-26 12:34:49 -0200

    update

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,16 +2,3 @@ bind = "127.0.0.1:9000"
 workers = 3
 loglevel = "debug"
 logfile = "/home/certdigitalvale/app/certificadodigitalvale/logs/gunicorn_supervisor.log"
-#raw_env = [
-#    'ENVIRONMENT=production',
-#    'SECRET_TOKEN=nq#u^m&9gh9bgsq^!_5#s94$3-ynxw1!*4-f2_)e#c5m#!1-=u',
-#    'ALLOWED_HOST_URL=["*"]',
-#    'MEMCACHED_LOCATION=""',
-#    'DB_NAME=certificadodigitalvale',
-#    'DB_USERNAME=certdigitvale',
-#    'DB_PASSWORD=cvm547882',
-#    'DB_HOST=localhost',
-#    'DB_PORT=5432',
-#    'SOURCE_DIR=/home/certdigitalvale/app/certificadodigitalvale/source',
-#    'REPO_DIR=/home/certdigitalvale/app/certificadodigitalvale/',
-#    ]
