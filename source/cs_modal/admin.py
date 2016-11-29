9ee88747303e93c7f035611499418abf5b362d48
Michel <michel.rodrigues86@yahoo.com.br>
2016-11-29 18:18:33 -0200

    update

--- source/cs_modal/admin.py
+++ source/cs_modal/admin.py
@@ -7,11 +7,11 @@ from .models import CreditServiceModal
 class CreditServiceModalAdmin(admin.ModelAdmin):
     form = CreditServiceModalForm
 
-    list_display = ['título', 'id', 'name_id', 'serviço']
+    # list_display = ['título', 'id', 'name_id', 'serviço']
 
     # Não permite a adição de novos objetos no admin
     def has_add_permission(self, request):
-        return True
+        return False
 
     # Não permite a remoção de objetos no admin
     def has_delete_permission(self, request, obj=CreditServiceModal):
